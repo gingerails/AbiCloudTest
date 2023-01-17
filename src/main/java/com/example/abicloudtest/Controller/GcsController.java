60944cabe795e98db20f749460f764d0c67c1581
Abi Kunkle <458514@genpt.net>
2023-01-17 10:49:35 -0600

    gcs controller for testing connection to GCS

---
+++ src/main/java/com/example/abicloudtest/Controller/GcsController.java
@@ -0,0 +1,23 @@
+package com.example.abicloudtest.Controller;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GcsController {
+
+    @Value("gs://abis-test/my-file.txt")
+    private Resource gcsFile;
+
+    @GetMapping("/read")
+    public String readGcsFile() throws IOException {
+        return StreamUtils.copyToString(
+                gcsFile.getInputStream(),
+                Charset.defaultCharset());
+    }
+}
--- src/main/java/com/example/abicloudtest/Controller/HelloController.java
+++ src/main/java/com/example/abicloudtest/Controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/h")
     public String home() {
         return "Hello Docker World";
     }
